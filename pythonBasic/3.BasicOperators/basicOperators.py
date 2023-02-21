3b7fff889e1e31a617c4b8fc92c157bb39eb1057
borisbecode <boris.lorenzonetto@gmail.com>
2023-02-21 12:30:37 +0100

    coucou

---
+++ pythonBasic/3.BasicOperators/basicOperators.py
@@ -0,0 +1,27 @@
+a= 10
+b = 20
+additon = (a+b)
+print(additon)
+substraction = (b-a)
+print(substraction)
+multiplication= a*b
+print(multiplication)
+division = a/b
+print(division)
+floor_division = b // a
+floor_division_bis = 31 // 10
+""" le double // permet d'avoir un integer, chiffre rond  """
+simple_division = 21 / 7.4
+""" le simple me donnera un chiffre a virgule   """
+print("floor_division: ", floor_division)   
+print("floor_division_bis: ", floor_division_bis)
+print("division avec virgule : ",simple_division)
+modulus_bis = 21 % 2
+print("modulus_bis :", modulus_bis)
+exposant = a ** 2
+print(exposant)
+c = 20
+d = 30
+print( c == d )
+print ( c != d)
+print(a<b)
