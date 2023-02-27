9d2dda3d78cd853996277dc98973927ec74165e6
borisbecode <boris.lorenzonetto@gmail.com>
2023-02-27 13:50:27 +0100

    avec test les 5 prems

--- pythonChallenges/1.HelloWorld.py
+++ pythonChallenges/1.HelloWorld.py
@@ -1,9 +1,12 @@
-
+from __future__ import unicode_literals
 import unittest
 
 
-def Hello(name="World"):
-    print("Hello "+name)
+def Hello(name=""):
+    if name != "":
+        return f"Hello, {name}!"
+    else:
+        return f"Hello, World!"
 
 Hello()    
 
@@ -11,6 +14,37 @@ Hello()
 
 
 
+# -*- coding: utf-8 -*-
 
+
+
+class HelloWorldTests(unittest.TestCase):
+
+    def test_hello_without_name(self):
+        self.assertEqual(
+            'Hello, World!',
+            Hello()
+        )
+
+    def test_hello_with_sample_name(self):
+        self.assertEqual(
+            'Hello, Alice!',
+            Hello('Alice')
+        )
+
+    def test_hello_with_other_sample_name(self):
+        self.assertEqual(
+            'Hello, Bob!',
+            Hello('Bob')
+        )
+
+    def test_hello_with_umlaut_name(self):
+        self.assertEqual(
+            'Hello, Jürgen!',
+            Hello('Jürgen')
+        )
+
+if __name__ == '__main__':
+    unittest.main()
 
 
